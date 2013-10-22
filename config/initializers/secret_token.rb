9fb96c36a2d31c9b7f7418f2cd6242cb40f03775
adriel <apinzasm@gmail.com>
2013-10-22 17:48:18 -0500

    creada la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcif::Application.config.secret_token = '03ab661720be52c718cd68031389bfd24973a98763e16154a724cdc5b470e5b45ddbdc5a6d697436ec89c12ca03365068d3ab84f7dd1d3b3e2b9b7c46bde044a'
---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/var/www/mcif/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.mcif.sock"
+worker_processes 2
+timeout 30
