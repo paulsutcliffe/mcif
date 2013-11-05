51ed9e08a8de12f36b576666f7290cb13c87d7df
adriel <apinzasm@gmail.com>
2013-11-05 18:02:27 -0500

    Implementado los selects de noticias

--- app/models/noticia.rb
+++ app/models/noticia.rb
@@ -1,20 +1,21 @@
 #coding: utf-8
 class Noticia < ActiveRecord::Base
-  attr_accessible :categoria, :contenido, :destacado, :fecha, :link, :subtitulo, :tipo, :titulo, :subcategoria
+  attr_accessible :categoria, :contenido, :destacado, :fecha, :link, :subtitulo, :tipo, :titulo, :subcategoria, :imagenes_attributes, :videos_attributes
   extend FriendlyId
 
   has_many :charts
   has_many :videos
   has_many :imagenes
-  accepts_nested_attributes_for :imagenes, :allow_destroy => true
+ accepts_nested_attributes_for :imagenes, :allow_destroy => true
+ accepts_nested_attributes_for :videos, :allow_destroy => true
 
   scope :clase, ->(subcategoria) { where("subcategoria = ?", subcategoria) }
   scope :noticia, -> { where("categoria = ?", "Noticia") }
   scope :publicacion, -> { where("categoria = ?", "Publicación") }
 
   TIPOS = ['Boletín', 'Folletos', 'Declaraciones','Videos','Orientación','Reclamaciones']
-  CATEGORIAA = ['Noticia', 'Publicación']
-  SUBCATEGORIA = ['Mcif', 'Recomendados']
+  CATEGORIAS = ['Noticia', 'Publicación']
+  SUBCATEGORIAS = ['Mcif', 'Recomendados']
 
   friendly_id :titulo, use: :slugged
 end
