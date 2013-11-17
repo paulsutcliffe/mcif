cc756e971e1782c4ccdf7ec29d70c55fac7f49c4
Ricardo <marcorcp1@hotmail.com>
2013-11-17 17:34:03 -0500

    working

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  
+  $("ul#ticker01").liScroll({travelocity: 0.05}); 
   $('.li-delete').each(function() {
     btn_delete = $(this).parents('.admin-controls').next();
     btn_delete.appendTo($(this));
