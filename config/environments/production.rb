6c5cc02eebc61fc828a7ba385135ac8241b6c39e
Paul Sutcliffe <paul@kosmyka.com>
2013-11-26 01:51:00 -0500

    scroller scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Mcif::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['li-scroller.css', 'nested_form.js', 'scripts.js', 'jquery.li-scroller.1.0.js']
+  config.assets.precompile += ['li-scroller.css.scss', 'nested_form.js', 'scripts.js', 'jquery.li-scroller.1.0.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
