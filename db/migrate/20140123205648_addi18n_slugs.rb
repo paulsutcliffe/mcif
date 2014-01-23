c146f0b99fc80dcd0e8396b65e38fe0b34681692
Paul Sutcliffe <paul@kosmyka.com>
2014-01-23 16:03:09 -0500

    i18slugs columns

--- db/migrate/20140123205648_addi18n_slugs.rb
+++ db/migrate/20140123205648_addi18n_slugs.rb
@@ -1,5 +1,9 @@
 class Addi18nSlugs < ActiveRecord::Migration
   def up
+    add_column :miembros, :slug_en, :string
+    add_column :miembros, :slug_es, :string
+    add_column :noticias, :slug_en, :string
+    add_column :noticias, :slug_es, :string
     add_index :miembros, :slug_en
     add_index :miembros, :slug_es
     add_index :noticias, :slug_en
