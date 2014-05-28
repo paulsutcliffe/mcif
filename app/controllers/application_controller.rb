acebbd443b317c67486644012a7351ebf2da46ba
Paul Sutcliffe <paul@kosmyka.com>
2014-05-28 01:14:19 -0500

    ticker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def cargar_ticker_noticias
-    @ticker_noticias = Noticia.order(fecha: :desc)
+    @ticker_noticias = Noticia.order(fecha: :desc).limit(5)
   end
 
 
